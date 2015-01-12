e60b3377ddaa43667730bfa679c7bd2d9af46d98
Sebastian Zimmer <sebastian.zimmer@uni-koeln.de>
2015-01-12 18:03:07 +0100

    New debug.js

---
+++ debug.js
@@ -0,0 +1,26 @@
+// allows to debug this environment by adding all the source files
+// to cmdi maker instead of build files.
+// repo should be available as http source for this to work
+// and the correct url_prefix must be given below.
+// in cmdi maker, call addFile(<this_file>)
+var url_prefix = "http://localhost:88/";
+
+var source_scripts = [
+	"src/js/eldp_main.js",
+	"src/js/eldp_LanguagePacks.js",
+	"src/js/eldp_generator.js",
+	"src/js/eldp_forms.js",
+	"src/js/eldp_resources.js",
+	"src/js/eldp_actors.js",
+	"src/js/eldp_actor_languages.js",	
+	"src/js/eldp_sessions.js",
+	"src/js/eldp_sessions_rendering.js",
+	"src/js/eldp_output.js",
+];
+
+var source_stylesheet = "src/css/layout-eldp.css";
+
+addFiles(source_scripts, url_prefix);
+addFiles(source_stylesheet, url_prefix);
+
+

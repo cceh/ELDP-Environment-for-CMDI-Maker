404dfb1763d052133a1d6f9148e066c4d7dd0b60
Sebastian Zimmer <sebastian.zimmer@uni-koeln.de>
2015-04-01 11:20:04 +0200

    Build 1.9

--- src/js/eldp_main.js
+++ src/js/eldp_main.js
@@ -23,7 +23,7 @@ var eldp_environment = (function(){
 	my.name = "eldp";
 	my.id = "eldp";
 	my.title = "ELDP";
-	my.version = "1.8";
+	my.version = "1.9";
 	my.min_cmdi_maker_version = "2.9";
 	
 	my.workflow = [];
